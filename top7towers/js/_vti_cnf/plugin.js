e98c9ce118e043d291921a8b8705c868291a504a
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-16 09:43:07 +0300

    main commit

---
+++ top7towers/js/_vti_cnf/plugin.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Jun 2020 11:01:46 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|burj-khalifa.html jin-mao.html plaza.html mfc.html petronas-towers.html siurs-tower.html index.html taioe101.html
